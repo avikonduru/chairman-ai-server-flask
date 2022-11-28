303040bf67c45634f42d3cb634966030c5fd05dd
Avi Konduru <avikonduru@gmail.com>
2022-11-28 16:08:02 -0600

    changes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+import pandas as pd
+import numpy as np
+import openai
+from openai.embeddings_utils import get_embedding, cosine_similarity
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    if request.method == 'POST':
+        return 'Calling the Chairman AI Python GET route'
+    else:
+        return 'Calling the Chairman AI Python POST route'
